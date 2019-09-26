d14af03c4eec4e988b3ac46d7da609d9e2148f6d
Vinit <karansainivs@gmail.com>
2019-09-26 22:58:58 +0530

    sdf

--- app/src/main/java/com/bignerdranch/android/demo/ScheduleFragment.kt
+++ app/src/main/java/com/bignerdranch/android/demo/ScheduleFragment.kt
@@ -5,11 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.bignerdranch.android.demo.ViewModel.DailyScheduleResponseViewModel
 import com.bignerdranch.android.demo.databinding.FragmentDailyScheduleBinding
 
 class ScheduleFragment : Fragment() {
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding = FragmentDailyScheduleBinding.inflate(inflater, container, false)
+        val viewModel : DailyScheduleResponseViewModel by viewModels()
+        binding.viewmodel = viewModel
+        binding.lifecycleOwner = this
+
+        println("asdfasdff --->> ${viewModel.dailySchedule}")
+
         return binding.root
     }
 }
--- app/src/main/java/com/bignerdranch/android/demo/ViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.bignerdranch.android.demo
-
-import androidx.lifecycle.ViewModel
-import com.bignerdranch.android.demo.Data.FacultyRepository
-
-class ViewModel : ViewModel() {
-    val facultyRepository = FacultyRepository()
-
-}
